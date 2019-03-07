7cdfee847fc725f3df64a90f8aa2d22377d94ce5
shaniqua simpson <niquacharisdesigns@gmail.com>
2019-03-07 00:20:13 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 /* Enter the code to remove the main node element under this comment */
-
+document.querySelector ('main')
+let myMain = document.querySelector ('main')
+myMain.remove()
 /* Create your new element here and assign it to newHeader */
-const newHeader = null;
+const newHeader = document.createElement("h1")
+newHeader.id = "victory"
+newHeader.innerText="Niqua is the champion!"
+document.body.appendChild(newHeader)
